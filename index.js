0b1828dafc4704717a26b8a608a971fa8774185e
anvpeace <cruzpeacem@gmail.com>
2022-08-13 15:30:15 -0400

    water conversion button working

--- index.js
+++ index.js
@@ -2,3 +2,21 @@
 
 // 
 
+let water = 0
+//on click of convert button waterEl gets value client put in 
+//that value will get added to the water variable
+const waterAmount= document.getElementById("water-amount")
+const waterConvert= document.getElementById("water-convert") 
+
+// once added to the water variable a function will be preformed to multiply the value by 2 
+waterConvert.addEventListener("click", function (){
+
+    water += waterAmount.value
+    let rice = water / 2
+    console.log(`rice needed: ${rice} cup(s)`)
+    water+= 0
+})
+
+
+
+
